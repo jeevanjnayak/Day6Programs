3d1dff149e8c17a371d4c2e522ce40aafc181221
jeevan <jeevanjnayak@gmail.com>
2022-01-29 17:59:18 +0530

    reversing a number

---
+++ NumReverse.java
@@ -0,0 +1,24 @@
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class SumZero {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter how many coupon codes you need: ");
+        int N=sc.nextInt();
+        
+        int Remainder = 1, Reverse = 0;
+        
+        while (N > 0) {
+            
+            Remainder = N % 10;
+            N /= 10;
+            Reverse = Reverse * 10 + Remainder;
+        }
+        
+        System.out.println(Reverse);
+    }
+}
